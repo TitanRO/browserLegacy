c8ef74283417881cb5da7aa06583f9fbf3367472
trojal <trojal@gmail.com>
2024-09-30 13:48:05 +0000

    Comments

--- src/Plugins/DefaultSettings/DefaultSettings.js
+++ src/Plugins/DefaultSettings/DefaultSettings.js
@@ -1,5 +1,15 @@
+/**
+ * DefaultSettings Plugin
+ *
+ * Framework to override default settings for ROBrowser without modifying the code.
+ *
+ * This file is a plugin for ROBrowser, (http://www.robrowser.com/).
+ *
+ * @author Trojal
+ */
 define(function( require )
 {
+    // Dependencies
     var Preferences = require('Core/Preferences');
 	var Audio = require('Preferences/Audio');
 	var Map = require('Preferences/Map');
@@ -7,20 +17,21 @@ define(function( require )
 	return function Init(){
         const customAudio = Preferences.get( 'Audio', {
             BGM:   {
-                volume: 0.1 // 0.5
+                volume: 0.1 // Defaults to 0.5
             },
             Sound: {
-                volume: 0.1 // 0.5
+                volume: 0.1 // Defaults to 0.5
             }
         }, 1.0 );
         Audio.BGM.volume = customAudio.BGM.volume;
         Audio.Sound.volume = customAudio.Sound.volume;
 
         const customMap = Preferences.get( 'Map', {
-            fog: false // true
+            fog: false // Defaults to true
         }, 1.1 );
         Map.fog = customMap.fog;
 
+        // Return true to signal successful initialization
 		return true;
 	}
 });
